dfd5753379b44fe384fc3adc4ca1bc0d308ddbb9
Stanislav Alexandrov <s.aleksandrov@zonatelecom.ru>
2020-07-17 00:01:31 +0300

    allow to print bits for multiple numbers

--- main.c
+++ main.c
@@ -2,19 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define make_strarr(name, str) typeof(*(str))(*name)[ strlen(str) + 1 ] = (void*)str;
+/* Creates a pointer to an array from null-terminated string */
+#define make_arrview_strlen(_name_, _str_) typeof(*(_str_))(*_name_)[ strlen(_str_) + 1 ] = (void*)(_str_);
 
 int main(int argc, char **argv) {
 	const make_array_ptr(args, argv, argc);
 
-	if(ARRAY_SIZE(args) != 2)
-		return printerrln("Usage: print_bits 0x1020"), EXIT_FAILURE;
+	if(ARRAY_SIZE(args) < 2)
+		return printerrln("Usage: print_bits 0x1020 0xaefd"), EXIT_FAILURE;
 
-	make_strarr(s, arr(args)[1]);
+	make_arrview_cfront(numbers, 1, args);
+	foreach_array_ref(numbers, ref) {
+		make_arrview_strlen(s, *ref);
 
-	const int base = (ARRAY_SIZE(s) > 3 && arr(s)[0] == '0' && (arr(s)[1] == 'x' || arr(s)[1] == 'X')) ? 16 : 10;
-	const unsigned long long v[1] = { strtoull(*s, NULL, base) };
-	print_array_bits(v);
+		const int base = (ARRAY_SIZE(s) > 3 && arr(s)[0] == '0' && (arr(s)[1] == 'x' || arr(s)[1] == 'X')) ? 16 : 10;
+		const unsigned long long v[1] = { strtoull(*s, NULL, base) };
+
+		print_array_bits(v);
+	}
 
 	return 0;
 }
