a122623af1856dd86e99bf63786d949b0685a264
Stanislav Alexandrov <s.aleksandrov@zonatelecom.ru>
2020-07-17 10:37:04 +0300

    remove useless checks

--- main.c
+++ main.c
@@ -2,9 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* Creates a pointer to an array from null-terminated string */
-#define make_arrview_strlen(_name_, _str_) typeof(*(_str_))(*_name_)[ strlen(_str_) + 1 ] = (void*)(_str_);
-
 int main(int argc, char **argv) {
 	const make_array_ptr(args, argv, argc);
 
@@ -13,11 +10,7 @@ int main(int argc, char **argv) {
 
 	make_arrview_cfront(numbers, 1, args);
 	foreach_array_ref(numbers, ref) {
-		make_arrview_strlen(s, *ref);
-
-		const int base = (ARRAY_SIZE(s) > 3 && arr(s)[0] == '0' && (arr(s)[1] == 'x' || arr(s)[1] == 'X')) ? 16 : 10;
-		const unsigned long long v[1] = { strtoull(*s, NULL, base) };
-
+		const unsigned long long v[1] = { strtoull(*ref, NULL, 0) };
 		print_array_bits(v);
 	}
 
