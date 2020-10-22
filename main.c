16b96b7a7461bfb1be94e598526ab3f65dd0944a
rustedsword <lightofmysoul@gmail.com>
2020-10-23 00:15:32 +0300

    add --help option

--- main.c
+++ main.c
@@ -5,14 +5,12 @@
 int main(int argc, char **argv) {
 	const make_array_ptr(args, argv, argc);
 
-	if(ARRAY_SIZE(args) < 2)
+	if(ARRAY_SIZE(args) < 2 || !strcmp("--help", arr(args)[1]) )
 		return printerrln("Usage: print_bits 0x1020 0xaefd"), EXIT_FAILURE;
 
 	make_arrview_cfront(numbers, 1, args);
-	foreach_array_ref(numbers, ref) {
-		const unsigned long long v[1] = { strtoull(*ref, NULL, 0) };
-		print_array_bits(v);
-	}
+	foreach_array_ref(numbers, ref)
+		print_array_bits( (unsigned long long [1]){ strtoull(*ref, NULL, 0) } );
 
 	return 0;
 }
